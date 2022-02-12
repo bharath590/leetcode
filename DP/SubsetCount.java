f068387e10e563cd8446d5708984fe2e95b6d5eb
bharath <chinnabalu590@gmail.com>
2022-02-12 14:55:27 -0600

    subsetcount knapsack variation

---
+++ DP/SubsetCount.java
@@ -0,0 +1,33 @@
+package com.company.leetcode.DP;
+
+public class SubsetCount {
+    public static void main(String[] args) {
+        int nums[] = {2, 3, 5, 6, 8, 10};
+        int N = nums.length;
+        int sum = 10;
+        int total = subsetCount(nums,N,sum);
+        System.out.println(total);
+    }
+    public static int subsetCount(int nums[],int N,int W){
+        int dp[][] = new int[N+1][W+1];
+        for (int i=0;i<N+1;i++){
+            for (int j=0;j<W+1;j++){
+                if(i==0){
+                    dp[i][j] = 0;
+                }
+                if(j==0){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for (int i=1;i<N+1;i++ ){
+            for (int j=1;j<W+1;j++){
+                if(nums[i-1]<=j)
+                     dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[N][W];
+    }
+}
